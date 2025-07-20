f969195d5f7dbae8eaa2f3f843c7f5d4d3cd4d18
wongju65 <justinmatthew.wong@mail.utoronto.ca>
2025-07-20 12:55:07 -0400

    alpha-beta pruning + simple move ordering

--- src/ChessAI/MinimaxBot.cpp
+++ src/ChessAI/MinimaxBot.cpp
@@ -3,55 +3,94 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int MAX_DEPTH = 5;
+constexpr int MAX_DEPTH = 6;
 constexpr int INF = 999999;
 
 int nodes_visited;  // For debugging/logging purpose only
 
+
+void MinimaxBot::order_moves(vector<tuple<int, int, int>> &moves, const Color self_turn, bool ascending) const {
+    ranges::sort(moves, [this, self_turn, ascending](const tuple<int, int, int> &a, const tuple<int, int, int> &b) {
+        const auto [from_a, to_a, promote_a] = a;
+        const auto [from_b, to_b, promote_b] = b;
+        engine.move(from_a, to_a, promote_a);
+        const int score_a = state_eval(engine.get_state(), self_turn);
+        engine.unmove();
+        engine.move(from_b, to_b, promote_b);
+        const int score_b = state_eval(engine.get_state(), self_turn);
+        engine.unmove();
+        if (ascending) return score_a < score_b;
+        return score_a > score_b;
+    });
+}
+
+
 // [[from, to, promote], score]
 pair<tuple<int, int, int>, int> MinimaxBot::minimax_search(
-    const Color self_turn, const Color current_turn, const int depth) {
+    const Color self_turn, const Color current_turn, const int depth, int alpha, int beta) {
 
     nodes_visited++;
 
     // Max depth reached
+    uniform_int_distribution<> noise(-1, 1);
     if (depth == MAX_DEPTH) {
-        return {{}, state_eval(engine.get_state(), self_turn)};
+        return {{}, state_eval(engine.get_state(), self_turn) + noise(rng)};
     }
 
     // Game ended
-    const vector<tuple<int, int, int>> moves = engine.get_legal_moves();
+    vector<tuple<int, int, int>> moves = engine.get_legal_moves();
     if (moves.empty()) {
-        if (game_end(engine) == 1) return {{}, self_turn == current_turn ? -INF + depth : INF - depth};
+        if (game_end(engine) == 1) return {{}, self_turn == current_turn ? -INF + depth * 5 : INF - depth * 5};
         return {};
     }
 
     // Recursive search
-    vector<tuple<int, int, int>> best_moves;
-    int best_score = (self_turn == current_turn ? -INF : INF);
-    for (auto [from, to, promote] : moves) {
-        engine.move(from, to, promote);
-        const Color next_turn = current_turn == Color::White ? Color::Black : Color::White;
-        auto [move, score] = minimax_search(self_turn, next_turn, depth + 1);
-        engine.unmove();
+    tuple<int, int, int> best_move;
+    if (self_turn == current_turn) {  // Maximizing
+        order_moves(moves, self_turn, false);
+        int best_score = -INF;
+        for (auto [from, to, promote] : moves) {
+            engine.move(from, to, promote);
+            const Color next_turn = current_turn == Color::White ? Color::Black : Color::White;
+            auto [move, score] = minimax_search(self_turn, next_turn, depth + 1, alpha, beta);
+            engine.unmove();
 
-        if ((current_turn == self_turn && score > best_score) || (current_turn != self_turn && score < best_score)) {
-            best_score = score;
-            best_moves.clear();
+            if (score > best_score) {
+                best_score = score;
+                best_move = {from, to, promote};
+            }
+
+            if (best_score >= beta) break;
+            alpha = max(alpha, best_score);
         }
-        if (score == best_score) {
-            best_moves.emplace_back(from, to, promote);
+
+        return {best_move, best_score};
+    } else {  // Minimizing
+        order_moves(moves, self_turn, true);
+        int best_score = INF;
+        for (auto [from, to, promote] : moves) {
+            engine.move(from, to, promote);
+            const Color next_turn = current_turn == Color::White ? Color::Black : Color::White;
+            auto [move, score] = minimax_search(self_turn, next_turn, depth + 1, alpha, beta);
+            engine.unmove();
+
+            if (score < best_score) {
+                best_score = score;
+                best_move = {from, to, promote};
+            }
+
+            if (best_score <= alpha) break;
+            beta = min(beta, best_score);
         }
+
+        return {best_move, best_score};
     }
-
-    uniform_int_distribution<> random_index(0, static_cast<int>(best_moves.size() - 1));
-    return {best_moves[random_index(rng)], best_score};
 }
 
 
 tuple<int, int, int> MinimaxBot::bestmove(string command) {
     nodes_visited = 0;
-    auto [move, score] = minimax_search(engine.get_turn(), engine.get_turn(), 0);
+    auto [move, score] = minimax_search(engine.get_turn(), engine.get_turn(), 0, -INF, INF);
     cout << "info string Minimax score: " << score << "\n";
     cout << "info string Minimax nodes: " << nodes_visited << "\n";
     return move;
--- src/ChessAI/MinimaxBot.h
+++ src/ChessAI/MinimaxBot.h
@@ -5,8 +5,10 @@
 #include <random>
 
 class MinimaxBot final : public AbstractBot {
+    void order_moves(std::vector<std::tuple<int, int, int>> &moves, Color self_turn, bool ascending) const;
     [[nodiscard]] std::pair<std::tuple<int, int, int>, int> minimax_search(
-        Color self_turn, Color current_turn, int depth);
+        Color self_turn, Color current_turn, int depth, int alpha, int beta);
+
 public:
     MinimaxBot() = delete;
     using AbstractBot::AbstractBot;
