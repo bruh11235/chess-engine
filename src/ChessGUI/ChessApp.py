4cf0e4e16aadae2b27120cbeaa64c8f4a5b81456
wongju65 <justinmatthew.wong@mail.utoronto.ca>
2025-08-07 16:41:31 -0400

    Removed Python files

--- src/ChessGUI/ChessApp.py
+++
@@ -1,9 +0,0 @@
-"""
-The main chess program.
-"""
-from ChessController import ChessController
-from ChessView import ChessView
-from ChessModel import ChessModel
-
-c = ChessController(ChessModel(), ChessView())
-c.start()
